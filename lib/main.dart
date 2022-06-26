7a17a832b47f6c455954a1f69403b8abd518f177
Matthias Kargl <matthias-kargl@t-online.de>
2022-06-26 16:33:47 -0700

    more apple files, no success w workmanager

--- lib/main.dart
+++ lib/main.dart
@@ -10,44 +10,47 @@ import '/notifications/notification_api.dart';
 
 void callbackDispatcher() {
   Workmanager().executeTask((taskName, inputData) async {
+    // await NotificationApi.cancel();
 
-    NotificationApi.cancel();
+    // final prefs = await SharedPreferences.getInstance();
+    // if ((prefs.getInt('numNot') != null) &&
+    //     (prefs.getInt('startT')! != null) &&
+    //     (prefs.getInt('endT') != null) &&
+    //     (prefs.getString('currentStudyPackageString') != null)) {
+    //   final int numNot = prefs.getInt('numNot')!;
+    //   final int startT = prefs.getInt('startT')!;
+    //   final int endT = prefs.getInt('endT')!;
+    //   final String dataBaseKey = prefs.getString('currentStudyPackageString')!;
 
-    final prefs = await SharedPreferences.getInstance();
-    if((prefs.getInt('numNot') != null) && (prefs.getInt('startT')! != null) && (prefs.getInt('endT') != null) && (prefs.getString('currentStudyPackageString') != null)) {
-      final int numNot = prefs.getInt('numNot')!;
-      final int startT = prefs.getInt('startT')!;
-      final int endT = prefs.getInt('endT')!;
-      final String dataBaseKey = prefs.getString('currentStudyPackageString')!;
-      final bool didUpdate = await staticFunction.scheduleNotificationsPerDay(endT, startT, numNot, dataBaseKey, false);
-      print('did update' + didUpdate.toString());
-    }
+    //   bool didUpdate = await staticFunction.scheduleNotificationsPerDay(
+    //       endT, startT, numNot, dataBaseKey, false);
+    //   print('did update' + didUpdate.toString());
+    // }
 
     final now = DateTime.now();
-    print("periodic task next in" + (24-now.hour).toString());
-    Workmanager().registerOneOffTask(
-        "dailyNotificationSchedule",
-        "backUp",
-        initialDelay: Duration(hours: 24-now.hour),
-        existingWorkPolicy: ExistingWorkPolicy.append
-    );
+    print("periodic task next in" + (24 - now.hour).toString());
+    // Workmanager().registerOneOffTask("dailyNotificationSchedule", "backUp",
+    //     initialDelay: Duration(minutes: 1), //hours: 24 - now.hour),
+    //     existingWorkPolicy: ExistingWorkPolicy.append);
     return Future.value(true);
   });
 }
 
-Future main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Workmanager().initialize(callbackDispatcher);
+  await Workmanager().initialize(
+    callbackDispatcher,
+    isInDebugMode: true,
+  );
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
 
-
-  Workmanager().registerOneOffTask(
-      "dailyNotificationSchedule",
-      "backUp",
-      //initialDelay: Duration(minutes: 1),
+  await Workmanager().registerOneOffTask(
+    "dailyNotificationSchedule",
+    "backUp",
+    tag: "task-identifier",
   );
 
   runApp(MyApp());
@@ -58,17 +61,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: title,
-    themeMode: ThemeMode.dark,
-    theme: ThemeData(
-      primaryColor: Colors.black,
-      scaffoldBackgroundColor: Colors.blueGrey.shade900,
-      appBarTheme: AppBarTheme(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-    ),
-    home: HomePage(),
-  );
+        debugShowCheckedModeBanner: false,
+        title: title,
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.blueGrey.shade900,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+        ),
+        home: HomePage(),
+      );
 }
